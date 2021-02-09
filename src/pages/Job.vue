60adccf83994b9d53dc7de69496e5dc17a6c94ec
Bret Hex Arimado <brethex@gmail.com>
2021-02-09 14:17:46 +0800

    Fix Job.vue

--- src/pages/Job.vue
+++ src/pages/Job.vue
@@ -2,7 +2,15 @@
 	<div>
 		Job
 		<div v-for="(job, index) in jobs" :key="index">
-			<div>{{ job.id }} {{ job.position }} {{ job.vacancy }}</div>
+			<div>{{ job.id }}</div>
+			<div>{{ job.program }}</div>
+			<div>{{ job.position }}</div>
+			<div>{{ job.vacancy }}</div>
+			<div>{{ job.area_of_assignment }}</div>
+			<div>{{ job.engagement_period }}</div>
+			<div>{{ job.salary_grade }}</div>
+			<div>{{ job.employment_status }}</div>
+			<div>{{ job.deadline }}</div>
 		</div>
 	</div>
 </template>
@@ -20,8 +28,14 @@
 				return entries.map(function (value) {
 					return {
 						id: value.gsx$id.$t,
+						program: value.gsx$program.$t,
 						position: value.gsx$position.$t,
-						vacancy: value.gsx$vacancy.$t
+						vacancy: value.gsx$vacancy.$t,
+						area_of_assignment: value.gsx$areaofassignment.$t,
+						engagement_period: value.gsx$engagementperiod.$t,
+						salary_grade: value.gsx$salarygrade.$t,
+						employment_status: value.gsx$employmentstatus.$t,
+						deadline: value.gsx$deadline.$t,
 					}
 				})
 			}
