7fbb6c038aa6d71ad2d9908b49f579a1208df4a2
Bret Hex Arimado <brethex@gmail.com>
2021-02-13 23:22:14 +0800

    Install vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
     <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+        <router-view/>
+
         <job-list />
     </div>
 </template>
@@ -32,3 +38,4 @@
         }
     }
 </script>
+
